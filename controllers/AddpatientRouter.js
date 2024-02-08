e5ebad7567319c632b62e2fa38359d512cb4db86
taniya-ignatius <itaniya895@gmail.com>
2024-02-08 22:48:33 +0530

    Patient searched

--- controllers/AddpatientRouter.js
+++ controllers/AddpatientRouter.js
@@ -14,5 +14,9 @@ router.get("/view",async(req,res)=>{
     let data=await addpatientModel.find()
     res.json(data)
 })
-
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await addpatientModel.find(input)
+    res.json(data)
+})
 module.exports=router
